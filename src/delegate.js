fdbc2ebfed821285f9e83336531c00213c740267
Dan Tello <dan.tello@viget.com>
2015-12-21 10:59:31 -0500

    Support useCapture param
    Resolves #3

--- src/delegate.js
+++ src/delegate.js
@@ -9,14 +9,14 @@ var closest = require('closest');
  * @param {Function} callback
  * @return {Object}
  */
-function delegate(element, selector, type, callback) {
+function delegate(element, selector, type, callback, useCapture) {
     var listenerFn = listener.apply(this, arguments);
 
-    element.addEventListener(type, listenerFn);
+    element.addEventListener(type, listenerFn, useCapture);
 
     return {
         destroy: function() {
-            element.removeEventListener(type, listenerFn);
+            element.removeEventListener(type, listenerFn, useCapture);
         }
     }
 }
