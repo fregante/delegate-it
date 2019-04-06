44570c3a4f204a759e3b966ba1a2fe84225ee670
Nick Taylor <nick@iamdeveloper.com>
2019-04-06 15:12:13 -0400

    Future proofed default export.

--- index.ts
+++ index.ts
@@ -1,20 +1,4 @@
-export type EventType = keyof GlobalEventHandlersEventMap;
-
-export type DelegateSubscription = {
-	destroy: VoidFunction;
-};
-
-export type Setup = {
-	selector: string;
-	type: EventType;
-	useCapture?: boolean | AddEventListenerOptions;
-}
-
-export type DelegateEventHandler<TEvent extends Event = Event, TElement extends Element = Element> = (event: DelegateEvent<TEvent, TElement>) => void;
-
-export type DelegateEvent<TEvent extends Event = Event, TElement extends Element = Element> = TEvent & {
-	delegateTarget: TElement;
-}
+import { DelegateEventHandler, EventType, DelegateSubscription, DelegateEvent, Setup } from './types';
 
 const elements = new WeakMap<EventTarget, WeakMap<DelegateEventHandler<any, any>, Set<Setup>>>();
 
@@ -159,8 +143,4 @@ function delegate<TElement extends Element = Element, TEvent extends Event = Eve
 	});
 }
 
-export default delegate;
-
-// For CommonJS default export support
-module.exports = delegate;
-module.exports.default = delegate;
+export = delegate;
