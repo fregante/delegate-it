5697313e3d1cddd2fd72fb45b7b1a64e0185d93c
Federico Brigante <me@fregante.com>
2023-04-22 13:27:07 +0800

    Add promised `oneEvent` listener (#41)

--- delegate.test.js
+++ delegate.test.js
@@ -1,27 +1,7 @@
 import test from 'ava';
 import sinon from 'sinon';
-import {JSDOM} from 'jsdom';
-import delegate from './index.js';
-
-const {window} = new JSDOM(`
-	<ul>
-		<li><a>Item 1</a></li>
-		<li><a>Item 2</a></li>
-		<li><a>Item 3</a></li>
-		<li><a>Item 4</a></li>
-		<li><a>Item 5</a></li>
-	</ul>
-`);
-
-global.Text = window.Text;
-global.Event = window.Event;
-global.Element = window.Element;
-global.Document = window.Document;
-global.MouseEvent = window.MouseEvent;
-global.AbortController = window.AbortController;
-global.document = window.document;
-const container = window.document.querySelector('ul');
-const anchor = window.document.querySelector('a');
+import {container, anchor} from './ava.setup.js';
+import delegate from './delegate.js';
 
 test.serial('should add an event listener', t => {
 	delegate(container, 'a', 'click', t.pass);
@@ -39,18 +19,14 @@ test.serial('should remove an event listener', t => {
 	delegate(container, 'a', 'click', spy, {signal: controller.signal});
 	controller.abort();
 
-	const anchor = document.querySelector('a');
 	anchor.click();
 	t.true(spy.notCalled);
 });
 
-test.serial('should pass an AbortSignal to an event listener', t => {
+test.serial('should not add an event listener of the controller has already aborted', t => {
 	const spy = sinon.spy();
-	const controller = new AbortController();
-	delegate(container, 'a', 'click', spy, {signal: controller.signal});
-	controller.abort();
+	delegate(container, 'a', 'click', spy, {signal: AbortSignal.abort()});
 
-	const anchor = document.querySelector('a');
 	anchor.click();
 	t.true(spy.notCalled);
 });
---
+++ one-event.ts
@@ -0,0 +1,66 @@
+import type {ParseSelector} from 'typed-query-selector/parser.d.js';
+import delegate, {
+	castAddEventListenerOptions,
+	type DelegateEvent,
+	type DelegateOptions,
+	type EventType,
+} from './delegate.js';
+
+/**
+ * Delegates event to a selector and resolves after the first event
+ */
+async function oneEvent<
+	Selector extends string,
+	TElement extends Element = ParseSelector<Selector, HTMLElement>,
+	TEventType extends EventType = EventType,
+>(
+	base: EventTarget | Document | Iterable<Element> | string,
+	selector: Selector,
+	type: TEventType,
+	options?: DelegateOptions
+): Promise<DelegateEvent<GlobalEventHandlersEventMap[TEventType], TElement>>;
+
+async function oneEvent<
+	TElement extends Element = HTMLElement,
+	TEventType extends EventType = EventType,
+>(
+	base: EventTarget | Document | Iterable<Element> | string,
+	selector: string,
+	type: TEventType,
+	options?: DelegateOptions
+): Promise<DelegateEvent<GlobalEventHandlersEventMap[TEventType], TElement>>;
+
+// This type isn't exported as a declaration, so it needs to be duplicated above
+async function oneEvent<
+	TElement extends Element,
+	TEventType extends EventType = EventType,
+>(
+	base: EventTarget | Document | Iterable<Element> | string,
+	selector: string,
+	type: TEventType,
+	options?: DelegateOptions,
+): Promise<DelegateEvent<GlobalEventHandlersEventMap[TEventType], TElement> | undefined> {
+	return new Promise(resolve => {
+		const listenerOptions = castAddEventListenerOptions(options);
+		listenerOptions.once = true;
+
+		if (listenerOptions.signal?.aborted) {
+			resolve(undefined);
+		}
+
+		listenerOptions.signal?.addEventListener('abort', () => {
+			resolve(undefined);
+		});
+
+		delegate(
+			base,
+			selector,
+			type,
+			// @ts-expect-error Seems to work fine
+			resolve,
+			listenerOptions,
+		);
+	});
+}
+
+export default oneEvent;
