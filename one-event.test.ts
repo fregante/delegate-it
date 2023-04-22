a13cd0e1c7b2fd67a4222a1128e23e469b560447
Federico Brigante <me@fregante.com>
2023-04-22 13:33:44 +0800

    Switch from AVA to Vitest (#42)

--- one-event.test.ts
+++ one-event.test.ts
@@ -1,25 +1,25 @@
-import test from 'ava';
-import {container, anchor} from './ava.setup.js';
+import {test, expect} from 'vitest';
+import {container, anchor} from './vitest.setup.js';
 import oneEvent from './one-event.js';
 
-test.serial('should resolve after one event', async t => {
+test('should resolve after one event', async t => {
 	const promise = oneEvent(container, 'a', 'click');
 	anchor.click();
 	const event = await promise;
-	t.true(event instanceof MouseEvent);
+	expect(event).toBeInstanceOf(MouseEvent);
 });
 
-test.serial('should resolve with `undefined` after it’s aborted', async t => {
+test('should resolve with `undefined` after it’s aborted', async t => {
 	const controller = new AbortController();
 	const promise = oneEvent(container, 'a', 'click', {signal: controller.signal});
 	controller.abort();
 
 	const event = await promise;
-	t.is(event, undefined);
+	expect(event).toBeUndefined();
 });
 
-test.serial('should resolve with `undefined` if the signal has already aborted', async t => {
+test('should resolve with `undefined` if the signal has already aborted', async t => {
 	const promise = oneEvent(container, 'a', 'click', {signal: AbortSignal.abort()});
 	const event = await promise;
-	t.is(event, undefined);
+	expect(event).toBeUndefined();
 });
