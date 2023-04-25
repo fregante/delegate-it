86bfc47ea08ea0dd34fcc576ba0ae93a2d489bde
Federico Brigante <me@fregante.com>
2023-04-25 19:53:01 +0800

    one-event

--- one-event.test.ts
+++ one-event.test.ts
@@ -1,9 +1,9 @@
 import {test, expect} from 'vitest';
-import {container, anchor} from './vitest.setup.js';
+import {anchor} from './vitest.setup.js';
 import oneEvent from './one-event.js';
 
 test('should resolve after one event', async t => {
-	const promise = oneEvent(container, 'a', 'click');
+	const promise = oneEvent('a', 'click');
 	anchor.click();
 	const event = await promise;
 	expect(event).toBeInstanceOf(MouseEvent);
@@ -11,7 +11,7 @@ test('should resolve after one event', async t => {
 
 test('should resolve with `undefined` after it’s aborted', async t => {
 	const controller = new AbortController();
-	const promise = oneEvent(container, 'a', 'click', {signal: controller.signal});
+	const promise = oneEvent('a', 'click', {signal: controller.signal});
 	controller.abort();
 
 	const event = await promise;
@@ -19,7 +19,7 @@ test('should resolve with `undefined` after it’s aborted', async t => {
 });
 
 test('should resolve with `undefined` if the signal has already aborted', async t => {
-	const promise = oneEvent(container, 'a', 'click', {signal: AbortSignal.abort()});
+	const promise = oneEvent('a', 'click', {signal: AbortSignal.abort()});
 	const event = await promise;
 	expect(event).toBeUndefined();
 });
